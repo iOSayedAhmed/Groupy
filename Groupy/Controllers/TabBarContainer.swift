97604ccd1216ea793ae92d190a6cb15abc0f62f1
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-08-11 00:00:26 +0200

    Apply user Defualt to share User info

---
+++ Groupy/Controllers/TabBarContainer.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarContainer.swift
+//  Groupy
+//
+//  Created by Develop on 10/08/2022.
+//  Copyright © 2022 Develop. All rights reserved.
+//
+
+import UIKit
+
+class TabBarContainer: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
