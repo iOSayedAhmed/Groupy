65c1868b57c74eb1de516d47e26b84fb424d173f
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-08-30 23:42:25 +0200

    Update ProfileVC.swift

--- Groupy/Controllers/ProfileVC.swift
+++ Groupy/Controllers/ProfileVC.swift
@@ -17,36 +17,72 @@ class ProfileVC: UIViewController {
     @IBOutlet weak var currenetPasswordTextField: UITextField!
     @IBOutlet weak var newPasswordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    
-    
+    var parameters = [String:String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "الصفحة الشخصية"
-        
-        nameTextField.underLined()
-        phoneNumberTextField.underLined()
-        emailTextField.underLined()
-        currenetPasswordTextField.underLined()
-        newPasswordTextField.underLined()
-        confirmPasswordTextField.underLined()	
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        guard let userInfo = Helpers.shared.defualts.object(forKey: "userInfo") as? [String:String] else {return}
-        
+        guard let userInfo = LocalState.userInfo as? [String:String] else {return}
         userNameLabel.text = userInfo["name"]
         userEmailLabel.text = userInfo["email"]
         nameTextField.text = userInfo["name"]
+        phoneNumberTextField.text = userInfo["phone"]
         emailTextField.text = userInfo["email"]
+    }
+    @IBAction func editUserImageClickedButton(_ sender: UIButton) {
+        print("editUserImageClickedButton")
+        
+    }
+    
+    @IBAction func updateUserInfoClickedButton(_ sender: UIButton) {
+        updateUserInfo()
+        
+        
     }
     
     @IBAction func ForgotPasswordClickedButton(_ sender: UIButton) {
+        print("ForgotPasswordClickedButton")
     }
     
     @IBAction func ResetPasswordClickedButton(_ sender: Any) {
+        
+        print("ResetPasswordClickedButton")
     }
     
     
-
+    
+}
+extension ProfileVC {
+    //MARK: - Functions
+    
+    func updateUserInfo(){
+        if let name = nameTextField.text , let email = emailTextField.text , let phone = phoneNumberTextField.text , let id = LocalState.userInfo["id"] as? String {
+            parameters = ["id":id ,"name":name,"email":email,"phone":phone]
+            
+            DispatchQueue.global(qos: .background).async {
+                APIServices.shared.postMSGRequest(url: "update_info_student.php", parameters: self.parameters) { responseMSG in
+                    if responseMSG == "0" {
+                        DispatchQueue.main.async {
+                            self.showAlert("تنبية ⚠️", "لم يتم تحديث البيانات", "تم")
+                        }
+                    }else {
+                        DispatchQueue.main.async {
+                            self.showAlert("تنبية", "تم تحديث البيانات بنجاح", "تم")
+                            self.userNameLabel.text = name
+                            self.userEmailLabel.text = email
+                            LocalState.userInfo["name"] = name
+                            LocalState.userInfo["email"] = email
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
 }
